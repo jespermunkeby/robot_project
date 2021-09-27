9e7e33d40481b3afa3540668c9db11eed8e8d5b5
jespermunkeby <46975845+jespermunkeby@users.noreply.github.com>
2021-09-27 15:47:41 +0200

    added function heads for robot.c

---
+++ src/robot/robot.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+float get_distance(){ //returns distance read from ultrasonic sensors in meters
+    return 0.;
+}
+
+bool get_touch(){ //returns true if toch sensor is pressed, false if not
+    return false;
+}
+
+float get_gyro(){ //returns gyro reading in radians
+    return 0.;
+}
+
+void drive(float dist){}
+
+void turn(){}
+
+void touch_align(){}
+
+void face_wall(){}
+
+void follow_wall(){}
+
+void drop(){}
