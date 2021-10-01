4d629262ded3daa055f71973a059c819b6439678
jespermunkeby <46975845+jespermunkeby@users.noreply.github.com>
2021-10-01 12:16:39 +0200

    updated robot.c with init and changed get sesor funcs

--- src/robot/robot.c
+++ src/robot/robot.c
@@ -12,37 +12,44 @@
 #define SENSOR_GYRO     IN4
 #define MOTOR_BOTH      ( MOTOR_LEFT | MOTOR_RIGHT ) /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
 
-
-float get_distance(){ //returns distance read from ultrasonic sensors in mm
-    if(sensor_is_plugged(SENSOR_DISTANCE, LEGO_EV3_US)){
-        return sensor_get_value0(SENSOR_DISTANCE,0);
+int init(){
+    if (! brick_init()) return ( 1 );
+    if (! sensor_is_plugged(SENSOR_TOUCH, LEGO_EV3_TOUCH)){
+        printf("Could not connect to SENSOR_TOUCH at socket IN1. Check if plugged in correctly");
+        return(0);
+    }
+    if (! sensor_is_plugged(SENSOR_DISTANCE, LEGO_EV3_US)){
+        printf("Could not connect to SENSOR_DISTANCE at socket IN2. Check if plugged in correctly");
+        return(0);
+    } 
+    if (! sensor_is_plugged(SENSOR_COLOR, LEGO_EV3_COLOR)){
+        printf("Could not connect to SENSOR_COLOR at socket IN3. Check if plugged in correctly");
+        return(0);
+    }
+    if (! sensor_is_plugged(SENSOR_GYRO, LEGO_EV3_GYRO)){
+        printf("Could not connect to SENSOR_GYRO at socket IN4. Check if plugged in correctly");
+        return(0);
     }
 
-    printf("Could not connect to SENSOR_DISTANCE at socket IN2. Check if plugged in correctly");
-    return 0.;
+    return(1);
+
+}
+
+
+float get_distance(){ //returns distance read from ultrasonic sensors in mm
+    return sensor_get_value0(SENSOR_DISTANCE,0);
 }
 
 bool get_touch(){ //returns true if toch sensor is pressed, false if not
-
-    if(sensor_is_plugged(SENSOR_TOUCH, LEGO_EV3_TOUCH)){
-        if(sensor_get_value0(SENSOR_TOUCH,0)){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    printf("Could not connect to SENSOR_TOUCH at socket IN1. Check if plugged in correctly");
-    return 0.;
+    if(sensor_get_value0(SENSOR_TOUCH,0)){
+        return true;
+    } else {
+        return false;
+    }    
 }
 
 float get_gyro(){ //returns gyro reading in radians
-    if(sensor_is_plugged(SENSOR_GYRO, LEGO_EV3_GYRO)){
-        return sensor_get_value0(SENSOR_GYRO,0);
-    }
-
-    printf("Could not connect to SENSOR_GYRO at socket IN4. Check if plugged in correctly");
-    return 0.;
+    return sensor_get_value0(SENSOR_GYRO,0);
 }
 
 void drive(float dist){}
@@ -51,18 +58,23 @@ void turn(){}
 
 void touch_align(){}
 
-void face_wall(){}
+void face_wall(){
 
-void follow_wall(){}
+}
+
+void follow_wall(){
+
+}
 
 void drop(){}
 
 int main(){
-    if ( !brick_init()) return ( 1 ); 
+    if (!init()) return ( 1 ); 
     
     while(true){
+        printf("\e[1;1H\e[2J");
         printf("distance: %f \n", get_distance());
-        /* printf("gyro: %f \n", get_gyro());
-        printf("touch: %d \n", int get_touch());*/
+        printf("gyro: %f \n", get_gyro());
+        printf("touch: %d \n", get_touch());
     }
 }
