64ca9d7d2363692289b229d886808c9cfc24eda8
jespermunkeby <46975845+jespermunkeby@users.noreply.github.com>
2021-10-06 13:07:05 +0200

    Gathered the groups code and put all functions together

--- src/robot/robot.c
+++ src/robot/robot.c
@@ -2,6 +2,7 @@
 #include "brick.h"
 #include <unistd.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define MOTOR_RIGHT     OUTA
 #define MOTOR_LEFT      OUTB
@@ -14,7 +15,7 @@
 
 float CALIBRATION_DRIVE_DISTANCE = 2;
 float CALIBRATION_TRIM = 0;
-float CALIBRATION_TURN = 1;
+float CALIBRATION_TURN = 1/0.5625;
 
 int init(){
     if (! brick_init()) return ( 0 );
@@ -52,54 +53,160 @@ bool get_touch(){ //returns true if toch sensor is pressed, false if not
     }    
 }
 
-float get_gyro(){ //returns gyro reading in radians
+float get_gyro(){ //returns gyro reading in degrees
     return sensor_get_value0(SENSOR_GYRO,0);
 }
 
 void drive(float dist){
-    tacho_set_position_sp(MOTOR_BOTH,dist*CALIBRATION_DRIVE_DISTANCE);
-    tacho_set_speed_sp(MOTOR_BOTH, tacho_get_max_speed(MOTOR_LEFT,0)*0.5);
-    tacho_run_to_rel_pos(MOTOR_BOTH);
-}
-
-void await_drive(){
+    tacho_reset(MOTOR_BOTH);
     
-}
-
-void turn_gyro(){}
+    float speed = tacho_get_max_speed(MOTOR_LEFT,0)*0.5;
 
 
-void turn_tacho(float deg){
-    tacho_set_position_sp(MOTOR_LEFT,deg*CALIBRATION_TURN);
-    tacho_set_position_sp(MOTOR_RIGHT,-deg*CALIBRATION_TURN);
-    tacho_set_speed_sp(MOTOR_BOTH, tacho_get_max_speed(MOTOR_LEFT,0)*0.2);
+    tacho_set_ramp_up_sp(MOTOR_BOTH, 3000);
+    tacho_set_ramp_down_sp(MOTOR_BOTH, 3000);
+
+    tacho_set_position_sp(MOTOR_BOTH,dist*CALIBRATION_DRIVE_DISTANCE);
+
+    tacho_set_speed_sp(MOTOR_BOTH, speed);
+
     tacho_run_to_rel_pos(MOTOR_BOTH);
+    
+    sleep(6);
+
+    for(;tacho_is_running(MOTOR_BOTH);){}
 }
 
-void touch_align(){}
+void turn_gyro(float deg){
+    tacho_reset(MOTOR_BOTH);
 
+    float sgn = (deg < 0)? -1 : 1;
+    float speed = tacho_get_max_speed(MOTOR_LEFT,0)*0.03;
 
-void face_wall(){
-    int smallest_element_index(float arr[], int length){
-        int index = 0;
-        int i;
-        for( i = 1; i < length; i++){
-            if(arr[i] < arr[index])
-                index = i;
-        }
-        return index;
-    }
+    float gyro_start = get_gyro();
 
+    tacho_set_stop_action_hold(MOTOR_BOTH);
+
+    tacho_set_speed_sp(MOTOR_LEFT, sgn*speed);
+    tacho_set_speed_sp(MOTOR_RIGHT, -sgn*speed);
+
+    tacho_run_forever(MOTOR_BOTH);
+
+    for( ; sgn*get_gyro() < sgn*(gyro_start+deg) ; ){}
 }
+
+void touch_align(){
+    int max_speed = tacho_get_max_speed( MOTOR_LEFT, 0 ); 
+    
+    tacho_set_speed_sp(MOTOR_BOTH, max_speed * 0.5); 
+
+    tacho_run_forever(MOTOR_BOTH);
+
+    while(!get_touch()){
+    } 
+
+    tacho_stop(MOTOR_BOTH);
+    
+    tacho_set_speed_sp(MOTOR_LEFT, max_speed * 0.6); 
+    tacho_set_speed_sp(MOTOR_RIGHT, max_speed * 0.4);
+
+    tacho_run_forever(MOTOR_BOTH);
+    
+    sleep(2);
+
+    //tacho_stop(MOTOR_BOTH);
+
+    tacho_set_speed_sp(MOTOR_LEFT, max_speed * 0.4); 
+    tacho_set_speed_sp(MOTOR_RIGHT, max_speed * 0.6);
+    tacho_run_forever(MOTOR_BOTH);
+
+    sleep(2);
+
+    tacho_set_speed_sp(MOTOR_BOTH, max_speed * 0.5); 
+    tacho_run_forever(MOTOR_BOTH);
+
+    sleep(1);
+
+    tacho_stop(MOTOR_BOTH);
+}
+
+
+void face_wall(){}
 
 void follow_wall(){
+    tacho_reset(MOTOR_BOTH);
 
+    float goal_distance = get_distance();
+    float fundamental_speed = tacho_get_max_speed(MOTOR_LEFT,0)*0.2;
+
+    tacho_set_ramp_up_sp(MOTOR_BOTH, 5000);
+    tacho_set_ramp_down_sp(MOTOR_BOTH, 5000);
+
+    tacho_set_speed_sp(MOTOR_BOTH, fundamental_speed);
+    tacho_run_forever(MOTOR_BOTH);
+
+    for (;;){
+        float current_distance = get_distance();
+        float steer = powf((current_distance - goal_distance)*0.03, 2); 
+        steer = (steer < 1) ? steer : 1; /*clamp below or eq to 1*/
+        float steer_amount = 0.3;
+
+        if(current_distance < goal_distance){
+            printf("RIGHT\n");
+            tacho_set_speed_sp(MOTOR_LEFT, fundamental_speed + fundamental_speed * steer_amount * steer);
+            tacho_set_speed_sp(MOTOR_RIGHT, fundamental_speed - fundamental_speed * steer_amount * steer);
+        } else {
+            printf("LEFT\n");
+            tacho_set_speed_sp(MOTOR_RIGHT, fundamental_speed + steer_amount*fundamental_speed*steer_amount*steer);
+            tacho_set_speed_sp(MOTOR_LEFT, fundamental_speed - steer_amount*fundamental_speed*steer_amount*steer);
+        }
+
+        tacho_run_forever(MOTOR_BOTH);
+        printf("%f\n\n", steer);
+    }
+}
+
+void follow_wall_pid(float kp, float ki, float kd){
+    tacho_reset(MOTOR_BOTH);
+
+    float target_distance = get_distance();
+    float fundamental_speed = tacho_get_max_speed(MOTOR_LEFT,0)*0.2;
+
+    tacho_set_speed_sp(MOTOR_BOTH, fundamental_speed);
+    tacho_run_forever(MOTOR_BOTH);
+
+    float proportional = 0;
+    float integral = 0;
+    float derivative = 0;
+
+    for (;;){
+        
+        float new_error = (get_distance() - target_distance);
+
+        proportional = (new_error)*kp;
+        integral = (integral+new_error)*ki;
+        derivative = (proportional - new_error)*kd;
+
+        float steer = proportional*kp + integral*ki + derivative*kd;
+        steer = steer < 1 ? steer : 1;
+        steer = steer > -1 ? steer : -1;
+
+        tacho_set_speed_sp(MOTOR_LEFT, fundamental_speed + fundamental_speed * steer);
+        tacho_set_speed_sp(MOTOR_RIGHT, fundamental_speed - fundamental_speed * steer);
+
+        tacho_run_forever(MOTOR_BOTH);
+        printf("p: %f\n", proportional);
+        printf("i: %f\n", integral);
+        printf("d: %f\n", derivative);
+        printf("steer: %f\n\n", steer);
+    }
 }
 
 void drop(){}
 
 int main(){
     if (!init()) return ( 1 ); 
-    turn_tacho(360*10);
-    sleep(60);
+
+    follow_wall_pid(0.05,0.05,0.05);
+
 }
