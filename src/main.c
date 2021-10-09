992f37967a5e83185fb042680d8063820ccd04e8
jespermunkeby <46975845+jespermunkeby@users.noreply.github.com>
2021-10-09 17:27:55 +0200

    added setup_orient to be implemented in missions with new delivery routes

--- src/main.c
+++ src/main.c
@@ -1,58 +1,67 @@
 #include "robot.h"
 
+void setup_orient(){
+    find_wall();
+    turn(-90);
+    touch_align();
+    drive(-400);
+}
+
+//TODO: Change missions to new delivery routes
+
 void mission_1(){
     find_wall();
-    turn_gyro(-90);
+    turn(-90);
     touch_align();
 
     drive(-200);
-    turn_gyro(90);
+    turn(90);
     
-    follow_wall_pid(0.05,0,0.5,-2500); //ska använda follow_wall_pd()
+    follow_wall_pid(-2500);
     drop();
     
-   //tillbaka
-    follow_wall_pid(0.05,0,0.5,2500);
+    /*tillbaka*/
+    follow_wall_pid(2500);
     turn(90);
     drive(300);
     
 }
 void mission_2(){
     find_wall();
-    turn_gyro(-90);
+    turn(-90);
     touch_align();
     
     drive(-200);
-    turn_gyro(90);
+    turn(90);
 
-    follow_wall_pid(0.05,0,0.5,2500); // ska använda follow_wall_pd()
+    follow_wall_pid(2500);
     drop();
     
-    //tillbaka    
-    follow_wall_pid(0.05,0,0.5,-2500);
+    /*tillbaka*/
+    follow_wall_pid(-2500);
     turn(-90);
     drive(300);   
 }
 
 void mission_3(){
     find_wall();
-    turn_gyro(-90);
+    turn(-90);
     touch_align();
     
     drive(-2500);
-    turn_gyro(180);
+    turn(180);
     touch_align();
 
     drive(-200);
-    turn_gyro(90);
+    turn(90);
 
-    follow_wall_pid(0.05,0,0.5,2500); // ska använda follow_wall_pd()
+    follow_wall_pid(2500);
     drop();
     
-    //tillbaka
-   follow_wall_pid(0.05,0,0.5,2500);
+    /*tillbaka*/
+   follow_wall_pid(2500);
     find_wall();
-    turn_gyro(90);
+    turn(90);
     
     touch_align();
     drive(-2500);
@@ -61,29 +70,30 @@ void mission_3(){
 
 void mission_4(){
     find_wall();
-    turn_gyro(-90);
+    turn(-90);
     touch_align();
     
     drive(-2500);
-    turn_gyro(180);
+    turn(180);
     touch_align();
 
     drive(-200);
-    turn_gyro(-90);
+    turn(-90);
 
-    follow_wall_pid(0.05,0,0.5,-2500); // ska använda follow_wall_pd()
+    follow_wall_pid(-2500);
     drop();
     
-    //tillbaka
-    follow_wall_pid(0.05,0,0.5,-2500);
+    /*tillbaka*/
+    follow_wall_pid(-2500);
     find_wall();
-    turn_gyro(-90);
+    turn(-90);
     
     touch_align();
     drive(-2500);
 }
 
-int main{
+int main(){
     if (!init()) return ( 1 ); 
-    
+    mission_1();
+    mission_2();
 }
